fb44da276ad25888edde2a31ab151c6bf93d321d
Derick Vigne <derickvigne@me.com>
2019-09-01 00:01:09 -0400

    Initial Commit and Config File

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "title": "A Plus Environmental Restoration Core Services Status Page",
+  "name": "a_plus_environmental_restoration_core_services_status_page",
+  "description": "Incident Status, Notifications, and Reporting for A Plus Environmental Restoration",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml",
+    "systems": [
+      "Main WebPage",
+      "Snipe-It",
+      "Mattermost",
+      "Single Sign-On",
+      "Mail Delivery and Relay",
+      "Infrastructure"
+    ],
+  }
+}
